127c54cab46db282762b31f323879b46812eb56f
MohamedHossamElden <mohamedhossamelden2009@gmail.com>
2023-03-05 20:07:11 +0200

    add files

---
+++ fb-login/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Form from "./components/form";
+import Text from "./components/text";
+
+const App = () => {
+  return (
+    <>
+      <Text />
+      <Form />
+    </>
+  );
+}
+
+export default App;
